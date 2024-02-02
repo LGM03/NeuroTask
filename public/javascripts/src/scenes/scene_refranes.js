69954838249887bd631dec50496416479b84a742
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-02-02 12:38:07 +0100

    refranes en proceso

--- public/javascripts/src/scenes/scene_refranes.js
+++ public/javascripts/src/scenes/scene_refranes.js
@@ -6,24 +6,46 @@ export default class scene_refranes extends Phaser.Scene {
         this.fallos = 0;
         this.fechaInicio = new Date();
         this.casos = ["Más vale estar solo que mal acompañado",
-            "Perro ladrador poco mordedor", "A caballo regalado no le mires el dentado", "A lo hecho pecho", "Al mal tiempo buena cara", 
-            "Cada loco con su tema","De tal palo tal astilla","El que la hace la paga", "Dinero llama dinero","El mundo es un pañuelo",
-            "Cada oveja con su pareja", "Más vale prevenir que lamentar" ,"Más vale tarde que nunca",
-            "En boca cerrada no entran moscas", "Al que madruga Dios le ayuda","El que calla otorga", "Amor con hambre no dura",
-            "Tira la piedra y esconde la mano","El que ríe el último ríe mejor"]
+            "Perro ladrador poco mordedor", "A caballo regalado no le mires el dentado", "A lo hecho pecho", "Al mal tiempo buena cara",
+            "Cada loco con su tema", "De tal palo tal astilla", "El que la hace la paga", "Dinero llama dinero", "El mundo es un pañuelo",
+            "Cada oveja con su pareja", "Más vale prevenir que lamentar", "Más vale tarde que nunca",
+            "En boca cerrada no entran moscas", "Al que madruga Dios le ayuda", "El que calla otorga", "Amor con hambre no dura",
+            "Tira la piedra y esconde la mano", "El que ríe el último ríe mejor"]
     }
 
-    init(data){
+    init(data) {
         this.idJuego = data.idJuego
     }
 
-    
     create() {
         const MS = 1000
         this.duracion = 20  //en segundos
-        this.time.delayedCall(this.duracion * MS , this.finalizarJuego, [], this);  //Finaliza el juego pasado el tiempo
-        
-        this.crearInterfaz(); 
+        this.time.delayedCall(this.duracion * MS, this.finalizarJuego, [], this);  //Finaliza el juego pasado el tiempo
+        this.crearInterfaz();
+
+    }
+
+    cubrirResultado(esAcierto) {
+        $('canvas').css('z-index', '2');
+        $('#juegoLenguaje').css('z-index', '1');
+        var color = 0xFF0000
+        if (esAcierto) {
+            var color = 0x00FF00
+        }
+        const cover = this.add.rectangle(0, 0, this.sys.game.config.width, this.sys.game.config.height, color, 0.5);
+        cover.setOrigin(0, 0);
+
+        this.tweens.add({
+            targets: cover,
+            alpha: 0,
+            duration: 1250,
+            ease: 'Linear',
+            onComplete: () => {
+                cover.destroy();
+                $('canvas').css('z-index', '1');
+                $('#juegoLenguaje').css('z-index', '2');
+            }
+        });
     }
 
     crearInterfaz() {
@@ -31,38 +53,64 @@ export default class scene_refranes extends Phaser.Scene {
         var elegido = Phaser.Math.Between(0, this.casos.length - 1)
         this.refran = this.casos[elegido] //Elijo el refran aleatoriamente
         //Para que no se repitan dos iguales en un mismo juego los elimino al sacarlos
-        if(this.casos.length!=1){
-            this.casos.splice(elegido,1)
+        if (this.casos.length != 1) {
+            this.casos.splice(elegido, 1)
         }
-
-        var arrayDePalabras =  this.refran.split(' '); //Desordeno la frase
-        arrayDePalabras.sort(function() {
+        let solucion = this.refran.split(' ')
+        var arrayDePalabras = this.refran.split(' '); //Desordeno la frase
+        arrayDePalabras.sort(function () {
             return 0.5 - Math.random();
         });
 
+        const self = this
         $('#juegoLenguaje').removeClass('d-none')
-        arrayDePalabras.forEach((element=>{
-            var botonPalabra = '<div class = "col" > <button class="btn botonPalabra rounded p-2 m-1 w-100 bg-white">'+element+'</button></div>'
+        arrayDePalabras.forEach((element => {
+            var botonPalabra = '<div class = "col" > <button class="btn botonPalabra rounded p-2 m-1 w-100 bg-white">' + element + '</button></div>'
             $('#contenedorBotones').append(botonPalabra)
         }))
-    
+
+        let fraseFormada = []
+        $(document).on("click", ".botonPalabra", function (event) {
+            fraseFormada.push($(this).text())
+            $("#fraseFormada").text($("#fraseFormada").text() + " " + $(this).text())
+            $(this).remove()
+        })
+
+        $("#btnCorregir").on("click", function (event) {
+            $("#contenedorBotones .botonPalabra").remove()
+            $("#fraseFormada").text("")
+            fraseFormada = []
+            arrayDePalabras.forEach((element => {
+                var botonPalabra = '<div class = "col" > <button class="btn botonPalabra rounded p-2 m-1 w-100 bg-white">' + element + '</button></div>'
+                $('#contenedorBotones').append(botonPalabra)
+            }))
+
+        })
+
+        $("#btnAceptar").on("click", function (event) {
+
+            if (fraseFormada.length != 0 && fraseFormada.length == solucion.length &&
+                fraseFormada.every((element, index) => element === solucion[index])) {
+                self.puntuacion++
+                self.cubrirResultado(true)
+
+            } else {
+                self.fallos++
+                self.cubrirResultado(false)
+            }
+
+            $("#contenedorBotones .botonPalabra").remove()
+            $("#fraseFormada").text("")
+            self.crearInterfaz();
+        })
     }
 
-    verificarRespuesta() {  //Colorear algo
-        if (this.refran== this.respuesta.text.trim()) {
-            console.log("Acierto")
-           this.puntuacion++
-        } else {
-            console.log("Fallo")
-            this.fallos++
-        }
-        this.respuesta.setText("")
-        this.palabrasGroup.clear(true,true)  //Vacio las letras de solucion y las palabras
 
-        this.crearInterfaz();
-    }
 
     finalizarJuego() {
+        $('canvas').css('z-index', '2');
+        $('#juegoLenguaje').css('z-index', '1');
+        $('#juegoLenguaje').addClass('d-none')
         const minutos = Math.floor(this.duracion / 60000);
         const segundos = (((this.duracion * 1000) % 60000) / 1000).toFixed(0);
 
@@ -73,17 +121,11 @@ export default class scene_refranes extends Phaser.Scene {
                 idJuego: this.idJuego,
                 fechaInicio: this.fechaInicio,
                 duracion: { minutos, segundos },
-                segundos : this.duracion
+                segundos: this.duracion
             });
     }
 
 }
 
-$(function(){
-    console.log("hola")
-    let fraseFormada = []
-    $(document).on("click",".botonPalabra",function(event){
-        fraseFormada.push($(this).text())
-        $("#fraseFormada").text( $("#fraseFormada").text()+ " "+$(this).text())
-    })
-})
+
+
