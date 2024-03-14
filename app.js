901f068da458048c5dba0d70c838669dc07dc058
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-03-14 22:10:32 +0100

    sesiones

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var app = express();
 const session = require("express-session");
 const mysqlSession = require("express-mysql-session");
 const MySQLStore = mysqlSession(session);
+const pool =  require('./routes/bd')
 
-const sessionStore = new MySQLStore({  //donde alamceno las sesiones
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "neurotask"
-});
-
-//Manejos de sesiones
+const sessionStore = new MySQLStore({
+  clearExpired: true,
+  checkExpirationInterval: 900000, // Intervalo de comprobación de expiración en milisegundos (15 minutos)
+  expiration: 86400000, // Tiempo de expiración predeterminado en milisegundos (1 día)
+},pool);
 
 const middlewareSession = session({
-  saveUninitialized: false,
-  secret: "foobar34",
-  resave: false,
-  store: sessionStore
+  secret: "foobar34", // Clave secreta para firmar la sesión
+  resave: false, // Evitar que se guarde la sesión si no se ha modificado
+  saveUninitialized: false, // Evitar que se guarde una sesión no inicializada
+  store: sessionStore // Almacenar sesiones en la base de datos MySQL
 });
+
+app.use(middlewareSession);
 
 var indexRouter = require('./routes/index');
 var juegoRouter = require('./routes/juego');
@@ -31,8 +31,7 @@ var usuarioRouter = require('./routes/usuario');
 var tareasRouter = require('./routes/tareas');
 var comentariosRouter = require('./routes/comentarios');
 
-var app = express();
-app.use(middlewareSession);
+
 
 // view engine setup
 app.use(express.static(path.join(__dirname, 'public')));
