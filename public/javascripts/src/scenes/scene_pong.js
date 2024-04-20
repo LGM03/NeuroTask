bea6f046dfbab2689fb0b87ab4e02543100eab33
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-04-20 11:12:35 +0200

    rename astronauta

--- public/javascripts/src/scenes/scene_pong.js
+++ public/javascripts/src/scenes/scene_pong.js
@@ -1,4 +1,4 @@
-import Palas from "../gameObjects/palas.js";
+import Obstaculo from "../gameObjects/obstaculo.js";
 export default class scene_pong extends Phaser.Scene {
 
     constructor() {
@@ -11,7 +11,7 @@ export default class scene_pong extends Phaser.Scene {
         this.nivel = data.nivel
         this.plan = data.plan
         this.RONDAS_TOTALES = 6
-        this.rondas_actuales = 0
+        this.rondas_actuales=0
 
         if (this.plan != null) {
             this.nivel = this.plan.nivel
@@ -20,8 +20,8 @@ export default class scene_pong extends Phaser.Scene {
 
     create() {
         const MS = 1000
-        this.puntosD = 0;
-        this.puntosI = 0;
+        this.fallos = 0;
+        this.aciertos = 0;
 
         let center_width = this.sys.game.config.width / 2;
         let center_height = this.sys.game.config.height / 2;
@@ -32,61 +32,63 @@ export default class scene_pong extends Phaser.Scene {
         var meta = this.add.image(30, center_height, "meta")
         meta.setScale(0.5)
 
-        this.derecha = new Palas(this, center_width * 2 - 30, center_height, "derecha")
-        this.derecha.setScale(0.8);
+        this.cohete = new Obstaculo(this, center_width * 2 - 30, center_height, "derecha")
+        this.cohete.setScale(0.8);
 
-        this.ball = this.physics.add.image(center_width, center_height, "ball")
-        this.ball.setVelocityX(500);
-        this.ball.setScale(this.sys.game.config.height * 0.2 / this.ball.height);
-        this.ball.setBounce(1); //Que rebote a la misma velocidad con la que choco
+        this.astronauta = this.physics.add.image(center_width, center_height, "ball")
+        this.astronauta.setVelocityX(500);
+        this.astronauta.setScale(this.sys.game.config.height * 0.2 / this.astronauta.height);
+        this.astronauta.setBounce(1); //Que rebote a la misma velocidad con la que choco
         this.physics.world.setBoundsCollision(false, false, true, true)//chocques con izq,derecha,arriba,abajo
-        this.ball.setCollideWorldBounds(true);
+        this.astronauta.setCollideWorldBounds(true);
 
-        this.izquierda2 = new Palas(this, width / 4, height / 6, "izquierda") //ok
-        this.izquierda2.setScale(0.3);
-        this.physics.add.collider(this.ball, this.izquierda2, this.chocaPala, null, this)
+        this.obstaculo2 = new Obstaculo(this, width / 4, height / 6, "izquierda") //ok
+        this.obstaculo2.setScale(0.3);
+        this.physics.add.collider(this.astronauta, this.obstaculo2, this.chocaPala, null, this)
 
         if (this.nivel > 1) {
-            this.izquierda3 = new Palas(this, width / 2, height - height / 6, "izquierda") //ok
-            this.izquierda3.setScale(0.3);
-            this.physics.add.collider(this.ball, this.izquierda3, this.chocaPala, null, this)
+            this.obstaculo3 = new Obstaculo(this, width / 2, height - height / 6, "izquierda") //ok
+            this.obstaculo3.setScale(0.3);
+            this.physics.add.collider(this.astronauta, this.obstaculo3, this.chocaPala, null, this)
         }
-        if (this.nivel == 3) {
-            crearTweenParaElemento.call(this, this.izquierda2, this.sys.game.config.height);
-            crearTweenParaElemento.call(this, this.izquierda3, this.sys.game.config.height / 6);
+        if(this.nivel==3){
+            crearTweenParaElemento.call(this, this.obstaculo2,this.sys.game.config.height);
+            crearTweenParaElemento.call(this, this.obstaculo3,this.sys.game.config.height / 6);
         }
-        this.physics.add.collider(this.ball, this.derecha, this.chocaPala, null, this)
+        this.physics.add.collider(this.astronauta, this.cohete, this.chocaPala, null, this)
     }
-
+    
 
     update(time, delta) {
-        if (this.ball.x < 0 || this.ball.x > this.sys.game.config.width) {
-            if (this.ball.x < 0) {
-                this.puntosI += 1;  // Corregir la variable
-
+        if (this.astronauta.x < 0 || this.astronauta.x > this.sys.game.config.width) {
+            if (this.astronauta.x < 0) {
+                this.aciertos += 1;  // Corregir la variable
+              
             } else {
-                this.puntosD += 1;  // Corregir la variable
-
+                this.fallos += 1;  // Corregir la variable
+                
             }
-            this.ball.setPosition(this.sys.game.config.width / 2, this.sys.game.config.height / 2);
-            this.ball.setVelocityX(500)
+            this.astronauta.setPosition(this.sys.game.config.width / 2, this.sys.game.config.height / 2);
+            this.astronauta.setVelocityX(500)
             this.rondas_actuales++;
 
         }
         // En el método create() o donde configures tus sprites y entradas del usuario
         this.input.on('pointerdown', function (pointer) {
-            this.derecha.y = pointer.y;
+                this.cohete.y =pointer.y
         }, this);
 
-        if (this.rondas_actuales == this.RONDAS_TOTALES) {
+    
 
+        if(this.rondas_actuales==this.RONDAS_TOTALES){
+        
             this.finalizarJuego()
         }
 
     }
 
     chocaPala() {
-        this.ball.setVelocityY(Phaser.Math.Between(-120, 120))
+        this.astronauta.setVelocityY(Phaser.Math.Between(-120, 120))
     }
 
 
@@ -98,8 +100,8 @@ export default class scene_pong extends Phaser.Scene {
         // Por ejemplo, puedes cambiar a otra escena
         this.scene.start("scene_fin",
             {
-                aciertos: this.puntosI,
-                fallos: this.puntosD,
+                aciertos: this.aciertos,
+                fallos: this.fallos,
                 idJuego: this.idJuego,
                 fechaInicio: this.fechaInicio,
                 duracion: { minutos, segundos },
@@ -113,7 +115,7 @@ export default class scene_pong extends Phaser.Scene {
 function crearTweenParaElemento(elemento, posY) {
     return this.tweens.add({
         targets: elemento,
-        y: posY,  // posición y hacia arriba
+        y: posY ,  // posición y hacia arriba
         duration: 2000,  // duración del movimiento en milisegundos
         yoyo: true,  // hace que el movimiento sea de ida y vuelta
         repeat: -1,  // para repetir infinitamente
