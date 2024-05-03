f7000afdbaed7781ba4e47763d4c5af22a557e92
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-05-03 19:14:54 +0200

    -env

---
+++ config.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv').config();
+
+module.exports = {
+    VAR_PORT: process.env.ENV_PORT || 3000,
+    VAR_HOST: process.env.ENV_HOST || "localhost",
+    VAR_USER: process.env.ENV_USER || "root",
+    VAR_PASSWORD: process.env.ENV_PASSWORD || "",
+    VAR_DATABASE: process.env.ENV_DATABASE || "neurotask"
+}
