6e9c8fcb3e332b09dba74ce24fad43f23cec34a6
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-05-08 12:06:08 +0200

    puerto especifico en bd

--- config.js
+++ config.js
@@ -5,5 +5,6 @@ module.exports = {
     VAR_HOST: process.env.ENV_HOST || "localhost",
     VAR_USER: process.env.ENV_USER || "root",
     VAR_PASSWORD: process.env.ENV_PASSWORD || "",
-    VAR_DATABASE: process.env.ENV_DATABASE || "neurotask"
+    VAR_DATABASE: process.env.ENV_DATABASE || "neurotask",
+    VAR_PORT_DB:  process.env.ENV_PORT_DB  || "3360"
 }
--- routes/bd.js
+++ routes/bd.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({ //instancia de acceso a la base de datos
   host: config.VAR_HOST,
   user: config.VAR_USER,
   password: config.VAR_PASSWORD,
-  database: config.VAR_DATABASE //nombre de la bd
+  database: config.VAR_DATABASE, //nombre de la bd
+  port : config.VAR_PORT_DB
 })
 
 module.exports = pool
