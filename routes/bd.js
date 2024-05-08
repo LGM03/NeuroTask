eb2a697e26627f929ae0e8671ddc48ab5e1b4f92
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-05-08 15:04:06 +0200

    prrueba para despliegue

--- routes/bd.js
+++ routes/bd.js
@@ -1,13 +1,13 @@
 const config = require('../config');
-const mysql = require("mysql")
+const mysql = require("mysql2")
 
 console.log(config)
 const pool = mysql.createPool({ //instancia de acceso a la base de datos 
-  host: config.VAR_HOST,
+  host:  config.VAR_HOST,
   user: config.VAR_USER,
-  password: config.VAR_PASSWORD,
-  database: config.VAR_DATABASE, //nombre de la bd
-  port : config.VAR_PORT_DB
+  password:config.VAR_PASSWORD,
+  database:config.VAR_DATABASE, //nombre de la bd
+  port :config.VAR_PORT_DB
 })
 
 module.exports = pool
