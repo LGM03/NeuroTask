a59e11e86b9ca45cdbec8726a94b669151348e08
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-05-08 12:41:44 +0200

    USO CORRECTO DE ENV

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    VAR_PORT: process.env.ENV_PORT || 3000,
-    VAR_HOST: process.env.ENV_HOST || "localhost",
-    VAR_USER: process.env.ENV_USER || "root",
-    VAR_PASSWORD: process.env.ENV_PASSWORD || "",
-    VAR_DATABASE: process.env.ENV_DATABASE || "neurotask",
-    VAR_PORT_DB:  process.env.ENV_PORT_DB  || "3360"
+    VAR_PORT: process.env.ENV_PORT ,
+    VAR_HOST: process.env.ENV_HOST ,
+    VAR_USER: process.env.ENV_USER ,
+    VAR_PASSWORD: process.env.ENV_PASSWORD ,
+    VAR_DATABASE: process.env.ENV_DATABASE,
+    VAR_PORT_DB:  process.env.ENV_PORT_DB  ,
 }
--- routes/bd.js
+++ routes/bd.js
@@ -1,11 +1,11 @@
-const config = require('../config');
+
 const mysql = require("mysql")
 const pool = mysql.createPool({ //instancia de acceso a la base de datos 
-  host: config.VAR_HOST,
-  user: config.VAR_USER,
-  password: config.VAR_PASSWORD,
-  database: config.VAR_DATABASE, //nombre de la bd
-  port : config.VAR_PORT_DB
+  host: process.env.VAR_HOST,
+  user:process.env.VAR_USER,
+  password: process.env.VAR_PASSWORD,
+  database: process.env.VAR_DATABASE, //nombre de la bd
+  port :process.env.VAR_PORT_DB
 })
 
 module.exports = pool
