eb31b2986818f39dec53051171d09098d843d6a2
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-05-08 13:30:10 +0200

    var env despligeue

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    VAR_PORT: process.env.ENV_PORT,
-    VAR_HOST: process.env.ENV_HOST,
-    VAR_USER: process.env.ENV_USER,
-    VAR_PASSWORD: process.env.ENV_PASSWORD,
-    VAR_DATABASE: process.env.ENV_DATABASE,
-    VAR_PORT_DB:  process.env.ENV_PORT_DB
+    VAR_PORT: process.env.VAR_PORT,
+    VAR_HOST: process.env.VAR_HOST,
+    VAR_USER: process.env.VAR_USER,
+    VAR_PASSWORD: process.env.VAR_PASSWORD,
+    VAR_DATABASE: process.env.VAR_DATABASE,
+    VAR_PORT_DB:  process.env.VAR_PORT_DB
 }
