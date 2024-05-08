3e9fcabb734179fdf5eadd1db864a97f1776cd65
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-05-08 14:14:05 +0200

    prueba

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    VAR_PORT: process.env.VAR_PORT,
+    PORT: process.env.PORT,
     VAR_HOST: process.env.VAR_HOST,
     VAR_USER: process.env.VAR_USER,
     VAR_PASSWORD: process.env.VAR_PASSWORD,
