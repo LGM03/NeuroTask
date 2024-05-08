81780c949474f68fb2067f2ab9ac37a036141b0b
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-05-08 13:08:27 +0200

    arreglos para despliegue

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    VAR_PORT: process.env.ENV_PORT ,
-    VAR_HOST: process.env.ENV_HOST ,
-    VAR_USER: process.env.ENV_USER ,
-    VAR_PASSWORD: process.env.ENV_PASSWORD ,
+    VAR_PORT: process.env.ENV_PORT,
+    VAR_HOST: process.env.ENV_HOST,
+    VAR_USER: process.env.ENV_USER,
+    VAR_PASSWORD: process.env.ENV_PASSWORD,
     VAR_DATABASE: process.env.ENV_DATABASE,
-    VAR_PORT_DB:  process.env.ENV_PORT_DB  ,
+    VAR_PORT_DB:  process.env.ENV_PORT_DB
 }
