ec21f4f576f7ab43eb4888bf6a3a3934ef1ce55e
LGM03 <99087911+LGM03@users.noreply.github.com>
2024-05-27 09:19:20 +0200

    configuracion

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const dotenv = require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT,
-    VAR_HOST: process.env.VAR_HOST,
-    VAR_USER: process.env.VAR_USER,
+    PORT: process.env.PORT || 3000,
+    VAR_HOST: process.env.VAR_HOST || localhost,
+    VAR_USER: process.env.VAR_USER || root,
     VAR_PASSWORD: process.env.VAR_PASSWORD,
-    VAR_DATABASE: process.env.VAR_DATABASE,
-    VAR_PORT_DB:  process.env.VAR_PORT_DB
+    VAR_DATABASE: process.env.VAR_DATABASE || neurotask,
+    VAR_PORT_DB:  process.env.VAR_PORT_DB || 3306
 }
